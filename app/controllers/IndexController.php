9917ca27ee3fcaf3990e07c6ce536abaaf7af009
shahid <shahid.rafiq@elementarylogics.com>
2017-06-01 14:06:20 +0500

    controller chnages

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,15 +12,15 @@ class IndexController extends ControllerBase
     }
     public function gitAction(){
         require_once('../vendor/gitclient/client/GitHubClient.php');
-       $data = $_POST;
-        if(!empty($data)){
-       $site_area =  $data['site_area'];
-       $desc =  $data['error_description'];
-       $os =  $data['operating_system'];
-       $user_agent =  $data['user_agent'];
-       $html_link =  $data['html_link'];
-       $date =  $data['submission_date'];
-       $error_message =  $data['error_message'];
+       $data = $this->request->getPost('error_description');
+        if($data != ''){
+       $site_area = $this->request->getPost('site_area');
+       $desc =  $this->request->getPost('error_description');
+       $os = $this->request->getPost('operating_system');
+       $user_agent =  $this->request->getPost('user_agent');
+       $html_link =  $this->request->getPost('html_link');
+       $date =  $this->request->getPost('submission_date');
+       $error_message =  $this->request->getPost('error_message');
 
 
         $body = $desc.'<br>Operation system:'.$os.'<br>User_agent:'.$user_agent.'<br>URL:'.$html_link.'<br>Date Submission:'.$date.'<br>Error message:'.$error_message;
@@ -37,6 +37,14 @@ class IndexController extends ControllerBase
         $client->setCredentials($username, $password);
 
         $testIssue = $client->issues->createAnIssue($owner, $repo, $title, $body);
+            //getting URL for the issue
+            //$Issue_Url = $testIssue->getHtmlUrl();
+
+            //Email body to be sent to admin
+            //$bodyEmail = $desc.'<br>Operation system:'.$os.'<br>User_agent:'.$user_agent.'<br>URL:'.$html_link.'<br>Date Submission:'.$date.'<br>Error message:'.$error_message.'<br>This issue can be found at the following URL:<br><a href="'.$Issue_Url.'"">'.$Issue_Url.'</a>';
+
+            /*send email code
+            admin@blackbusiness.website*/
         $this->flashSession->success("Issue Created Successfully");
         $this->response->redirect('');
         }else{
